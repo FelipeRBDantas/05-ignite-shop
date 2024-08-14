e274dc31db9754a40ecfd5ce63b5e98177de39dc
feliperbdantas <feliperbdantas@outlook.com>
2024-08-14 20:56:11 -0300

    feat: Initial Structure - Setting Up Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '12px 16px',
+  color: '#fff',
+  cursor: 'pointer',
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+
+  span: {
+    fontWeight: 'bold',
+  },
+})
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
   )
 }
