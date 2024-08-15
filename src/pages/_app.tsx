eb916f6e1603829fe5eceb636f13cf62ab93f3dc
feliperbdantas <feliperbdantas@outlook.com>
2024-08-14 21:26:05 -0300

    feat: Initial Structure - Application Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
 import { AppProps } from "next/app"
+
 import { globalStyles } from "../styles/global"
+
+import { Container, Header } from "../styles/pages";
+
+import logoImg from '../assets/logo.svg'
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { src, alt, width, height } = logoImg
+
+  return (
+    <Container>
+      <Header>
+        <img 
+          src={ src } 
+          height={ height } 
+          width={ width } 
+          alt={ alt } 
+        />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
