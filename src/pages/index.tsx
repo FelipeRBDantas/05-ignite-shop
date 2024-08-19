cc1dc2d1271e9a9b1a1a7a5bec89b93967c19b5b
feliperbdantas <feliperbdantas@outlook.com>
2024-08-19 20:14:42 -0300

    feat: Product & Checkout - Navigation via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import { stripe } from "../lib/stripe"
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import 'keen-slider/keen-slider.min.css'
+import Link from "next/link"
 
 interface HomeProps {
   products: {
@@ -33,15 +34,17 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={ sliderRef } className="keen-slider">
       { products.map(product => {
         return (
-          <Product key={ product.id } className="keen-slider__slide">
-            <Image src={ product.imageUrl } width={520} height={480} alt="" />
+          <Link key={ product.id } href={ `/product/${ product.id }` }>
+            <Product className="keen-slider__slide">
+              <Image src={ product.imageUrl } width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{ product.name }</strong>
+              <footer>
+                <strong>{ product.name }</strong>
 
-              <span>{ product.price }</span>
-            </footer>
-          </Product> 
+                <span>{ product.price }</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
