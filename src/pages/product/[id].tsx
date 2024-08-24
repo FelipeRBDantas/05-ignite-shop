9d89835129328ed8b7fcc6a224ca178f729fd031
feliperbdantas <feliperbdantas@outlook.com>
2024-08-23 21:34:31 -0300

    feat: Product & Checkout - Routes API in Next.js / Checkout in Stipe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,5 @@
 import Image from "next/image"
 
-import { useRouter } from "next/router"
-
 import { GetStaticPaths, GetStaticProps } from "next"
 
 import Stripe from "stripe"
@@ -21,14 +19,13 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
-  const { isFallback } = useRouter()
-
-  if (isFallback) {
-    return <p>Loading...</p>
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
   }
 
   return (
@@ -44,7 +41,9 @@ export default function Product({ product }: ProductProps) {
 
         <p>{ product.description }</p>
 
-        <button>Comprar agora</button>
+        <button onClick={ handleBuyProduct }>
+          Comprar agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -79,6 +78,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour
